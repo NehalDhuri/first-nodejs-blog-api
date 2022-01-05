992a45f996abd7a9dd029209799300512bf3c144
Nehal Dhuri <nehaldhuri221@gmail.com>
2022-01-05 20:02:47 +0530

    update

--- index.js
+++ index.js
@@ -95,5 +95,5 @@ app.delete('/posts/:id', function(req, res) {
 
 
 app.listen(process.env.PORT || 3003,function(){
-  console.log("Server is running on the port number 8000")
+  console.log("Server is running on the port number 3003")
 })
